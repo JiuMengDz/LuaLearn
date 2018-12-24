6f47df9cd611787c2e304280ef34990144fbbcf3
liudewei <326703305@qq.com>
2018-12-24 17:14:42 +0800

    将双向队列在moduleA文件中实现无损遍历，利用迭代器的方式

--- fouth.lua
+++ fouth.lua
@@ -1,2 +1,9 @@
-local m = require("moduleA")
-m.work("a", "b", 1, 2, 3)
+local DiQueen = require("moduleA")
+tb = DiQueen.init()
+
+DiQueen.addHead(tb, 50)
+DiQueen.addHead(tb, 30)
+DiQueen.addHead(tb, 10)
+for i in DiQueen.genertatorValue(tb) do
+    print(i)
+end
--- moduleA.lua
+++ moduleA.lua
@@ -1,11 +1,59 @@
-local M = {}    -- 通过table来实现模块
+local diQueen = {}
 
-M.work = function(...)
-    print("function working")
-    for i, v in ipairs{...} do
-          print(i, v)
-     end
-     -- do some job.
+function diQueen.init()
+     return {first = 0, last = -1}
 end
 
-return M
+function diQueen.addHead(table, value)
+     local pos = table.first - 1
+     table.first = pos
+     table[pos] = value
+end
+
+function diQueen.addTail(table, value)
+     local pos = table.last + 1
+     table.last = pos
+     table[pos] = value
+end
+
+function diQueen.popHead(table)
+     if (table.first > last) then
+          print("This queen is empty!")
+          return
+     end
+     print("The head of this queed is ", table[table.first])
+     table[table.first] = nil
+     tabel.first = table.first + 1
+end
+
+function diQueen.popTail(table)
+     if (tabel.first > last) then
+          print("This queen is empty!")
+          return
+     end
+     print("The tail of this queen is ", table[table.last])
+     table[tabel.last] = nil
+     table.last = table.last - 1
+end
+
+-- 无损遍历队列
+function diQueen.genertatorValue(table)
+     local first = table.first
+     local last = -1
+     return function()
+          if first < 0 then
+               local value = table[first]
+               first = first + 1
+               return value
+          else
+               if last < table.last then
+                    value = tabel[last]
+                    last = last + 1
+                    return value
+               end
+          end
+          return nil
+     end
+end
+
+return diQueen
