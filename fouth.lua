26f4c30f35d3dcf6eb72725fc820d92536d36e01
liudewei <326703305@qq.com>
2018-12-25 11:11:52 +0800

    通过代理的方式实现对表的跟踪访问

--- fouth.lua
+++ fouth.lua
@@ -1,7 +1,34 @@
-local metaTB = require("moduleA")
-s1 = metaTB.new{10,20,30,50}
-s2 = metaTB.new{30,1}
-s3 = (s1 + s2)*s1
-print(metaTB.tostring(s3))
+function track(t)
+    local proxy = {} --t的代理表
 
-print()
+    -- 为代理创建元表
+    local mt = {
+        __index = function(_, k)
+            print("*access to element " .. tostring(k))
+            return t[k] -- 返回原来的表
+        end,
+        __newindex = function(_, k, v)
+            print("*update of element " .. tostring(k) .. " to " .. tostring(v))
+            t[k] = v -- 更新原来的表
+        end,
+        __pairs = function()
+            return function(_, k) --迭代函数
+                local nextkey, nextvalue = next(t, k)
+                if nextkey ~= nil then --避免最后一个值
+                    print("*traversing element " .. tostring(nextkey))
+                end
+                return nextkey, nextvalue
+            end
+        end,
+        __len = function()
+            return #t
+        end
+    }
+
+    setmetatable(proxy, mt)
+    return proxy
+end
+
+t = track {}
+t[2] = "hello"
+print(t[2])
